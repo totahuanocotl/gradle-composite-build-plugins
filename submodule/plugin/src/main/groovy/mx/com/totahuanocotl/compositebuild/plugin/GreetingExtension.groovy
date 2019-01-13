cc04ddcae89ca107d17e2dacb16444d265f548fc
Isaac Martinez Aceves <isaac.martinez.aceves@gmail.com>
2019-01-13 18:47:11 +0000

    Using gradle 4.2

--- submodule/plugin/src/main/groovy/mx/com/totahuanocotl/compositebuild/plugin/GreetingExtension.groovy
+++ submodule/plugin/src/main/groovy/mx/com/totahuanocotl/compositebuild/plugin/GreetingExtension.groovy
@@ -1,15 +1,15 @@
 package mx.com.totahuanocotl.compositebuild.plugin
 
 import org.gradle.api.Project
-import org.gradle.api.provider.Property
+import org.gradle.api.provider.PropertyState
 
 class GreetingExtension {
-    final Property<String> greeting
-    final Property<String> recipient
+    final PropertyState<String> greeting
+    final PropertyState<String> recipient
 
     GreetingExtension(Project project) {
-        greeting = project.objects.property(String)
-        recipient = project.objects.property(String)
+        greeting = project.property(String)
+        recipient = project.property(String)
         greeting.set('Hello')
         recipient.set('Plugin Extension')
     }
--- submodule/plugin/src/main/groovy/mx/com/totahuanocotl/compositebuild/plugin/GreetingTask.groovy
+++ submodule/plugin/src/main/groovy/mx/com/totahuanocotl/compositebuild/plugin/GreetingTask.groovy
@@ -1,13 +1,13 @@
 package mx.com.totahuanocotl.compositebuild.plugin
 
 import org.gradle.api.DefaultTask
-import org.gradle.api.provider.Property
+import org.gradle.api.provider.PropertyState
 import org.gradle.api.tasks.TaskAction
 
 
 class Greeting extends DefaultTask {
-    final Property<String> greeting = project.objects.property(String)
-    final Property<String> recipient = project.objects.property(String)
+    PropertyState<String> greeting = project.property(String)
+    PropertyState<String> recipient = project.property(String)
 
     @TaskAction
     void greet() {
